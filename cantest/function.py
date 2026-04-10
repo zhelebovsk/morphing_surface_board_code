7474de87a863f82610dffef97d61cc13e535f960
Alexey Zhelebovskiy <alexey.zh@campus.technion.ac.il>
2026-04-10 18:51:30 +0300

    8hz 50mm function for the experiment

--- cantest/function.py
+++ cantest/function.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def motor_function(x_mm, y_mm, t):
-    return np.sin(2 * np.pi * (- t * 5 + x_mm / 60))
+    return np.sin(2 * np.pi * (- t * 8 + x_mm / 50))
     # return np.sin(t * 2 * np.pi/5 + x_mm / 100) * np.cos(t * 2 * np.pi/5 + y_mm / 100)
     # return -1.0
